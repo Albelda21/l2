0c715cbc160d4a7b457bfaaa725c88ed5d87ba40
albelda21 <kharinserhiy@gmail.com>
2018-06-20 23:30:02 +0300

    fix

--- app.js
+++ app.js
@@ -4,40 +4,15 @@ const path = require('path');
 const bodyParser = require('body-parser');
 
 
-// fake posts to simulate a database
-const content = [
-    {
-        id: 1,
-        author: 'John',
-        title: 'Templating with EJS',
-        body: 'Blog post number 1'
-    },
-    {
-        id: 2,
-        author: 'Drake',
-        title: 'Express: Starting from the Bottom',
-        body: 'Blog post number 2'
-    },
-    {
-        id: 3,
-        author: 'Emma',
-        title: 'Streams',
-        body: 'Blog post number 3'
-    },
-    {
-        id: 4,
-        author: 'Cody',
-        title: 'Events',
-        body: 'Blog post number 4'
-    }
-]
-
 // set the view engine to ejs
 app.set('view engine', 'ejs')
 app.use(express.static(path.join(__dirname, 'public')));
 
 // blog home page
 app.get('/', (req, res) => {
+
+    let domain = 'zazu';
+
     // render `home.ejs` with the list of posts
     res.render('home', { content: '<h1>Content</h1>' })
 })
